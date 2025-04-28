6a54a2141b37c58e424f9bab4657a09b648a7acb
Emeric Krause <ekrause@k0r3p4.42mulhouse.fr>
2025-04-28 17:47:35 +0200

    ex00

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -4,9 +4,11 @@ int main()
 {
 	std::cout << CYAN << "Zombie allocated on the stack:" << RESET << std::endl;
 	randomChump("Gerard Depardieu");
+
 	std::cout << CYAN << "Zombie allocated on the heap:" << RESET << std::endl;
 	Zombie *zombie = newZombie("Philippe Etchebest");
 	zombie->announce();
 	delete zombie;
+	
 	return (0);
 }

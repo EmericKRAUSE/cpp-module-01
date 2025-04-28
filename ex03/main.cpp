b13c99b078f5142ef0b59d9442e6adbf4ca9b208
Emeric Krause <ekrause@k0r3p4.42mulhouse.fr>
2025-04-28 18:23:00 +0200

    ex03

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -4,16 +4,20 @@
 
 int main()
 {
+	//-- Human A --//
 	Weapon club = Weapon("crude spiked club");
 	HumanA bob("Bob", club);
 	bob.attack();
 	club.setType("some other type of club");
 	bob.attack();
 
+	//-- Human B --//
 	Weapon sword = Weapon("shiny iron sword");
 	HumanB jim("Jim");
+	// Without weapon
 	jim.attack();
 	jim.setWeapon(&sword);
+	// With Weapon
 	jim.attack();
 	sword.setType("some other type of sword");
 	jim.attack();
